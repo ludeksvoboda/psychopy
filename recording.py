3805cf4fc966dee5b3800e5f9f3848693276261e
ludek <ludek.svoboda@abtera.cz>
2022-12-25 01:25:19 +0100

    changed saving point to two ints, droping first record, explored vid len in image count

--- recording.py
+++ recording.py
@@ -9,7 +9,6 @@ import pygame.camera
 import mysql.connector
 import config
 import numpy as np
-import os
 
 mydb = mysql.connector.connect(
             host="10.0.0.114",
@@ -25,6 +24,7 @@ def screen(child_conn):
     win = visual.Window([1920, 1080], monitor=monitor, units="deg", screen=0, fullscr=True)
     circle = visual.Circle(win, units="pix", edges=32, radius=8, fillColor='yellow')
     max = (960, 540)
+    i = 1
 
     while not event.getKeys('q'):
         x = random.randint(-max[0], max[0])
@@ -33,14 +33,15 @@ def screen(child_conn):
         circle.draw()
         win.flip(clearBuffer=True)
         time.sleep(1.5)
-        coordinates = f"POINT({x} {y})"
-        query = "INSERT INTO eye_click.recordings(coordinates) " \
-                "VALUES (ST_PointFromText(%(coordinates)s))"
-        mycursor.execute(query, {"coordinates": coordinates})
-        mydb.commit()
-        print('sending')
-        child_conn.send([x, y, mycursor.lastrowid])
-        #send info to video recorder
+        if i > 1:
+            query = "INSERT INTO eye_click_psychopy.data(x , y) " \
+                    "VALUES (%(x)s, %(y)s)"
+            mycursor.execute(query, {"x": x, "y": y})
+            mydb.commit()
+            child_conn.send([x, y, mycursor.lastrowid, i])
+        else:
+            child_conn.send([0, 0, 0, i])
+        i += 1
 
     win.close()
     core.quit()
@@ -50,17 +51,16 @@ def video(parent_conn):
     pygame.camera.init()
     cam = pygame.camera.Camera("/dev/video1", (640, 480))
     cam.start()
+    dir_name = '/home/lu/MLData/psychopyEyeClick/'
     images = []
 
     while(True):
         images.append(pygame.surfarray.array3d(cam.get_image()))
         if parent_conn.poll():
-                print("poll")
                 save_info = parent_conn.recv()
-                dir_name = '/home/lu/MLData/psychopyEyeClick/'+ str(save_info[2])
-                os.mkdir(dir_name)
-                np.save(dir_name +'/images.npy', images, allow_pickle=True)
-                images = []
+                if save_info[3] > 1:
+                    np.save(dir_name +'/images' + str(save_info[2]) + '.npy', images, allow_pickle=True)
+                images = [] 
 
 if __name__ == '__main__':
     p,q = Pipe()
