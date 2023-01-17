7aefdae52ae38af514ff871dc0c6e16d16e1c1cf
ludek <ludek.svoboda@abtera.cz>
2023-01-17 20:09:17 +0100

    added some notes

--- psychopy_tests.py
+++ psychopy_tests.py
@@ -8,13 +8,18 @@ win = visual.Window([1920, 1080], monitor=monitor, units="deg", screen=0, fullsc
 circle = visual.Circle(win, units="pix", edges=32, radius=8, fillColor='yellow')
 
 max = (960, 540)
+i = 1
 while not event.getKeys('q'):
-    x = random.randint(-max[0], max[0])
-    y = random.randint(-max[1], max[1])
-    circle.pos = (x,y)
+    # x = random.randint(-max[0], max[0])
+    # y = random.randint(-max[1], max[1])
+    if i % 2 == 0:
+        circle.pos = (max[0]-100,max[1]-100)
+    else:
+        circle.pos = (-max[0]+100,-max[1]+100)
     circle.draw()
     win.flip(clearBuffer=True)
     time.sleep(2)
+    i += 1
     #send info to video recorder
 
 win.close()
